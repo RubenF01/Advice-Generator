33a38d1968d8634bd7756daa60473e942e35ec10
Rubén Frías <rubenfrias@Rubens-MacBook-Pro.local>
2022-04-29 09:41:16 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Manrope", "sans-serif"],
+      },
+      colors: {
+        lightCyan: "hsl(193, 38%, 86%)",
+        neonGreen: "hsl(150, 100%, 66%)",
+        grayishBlue: "hsl(217, 19%, 38%)",
+        darkGrayishBlue: "hsl(217, 19%, 24%)",
+        darkBlue: "hsl(218, 23%, 16%)",
+      },
+    },
+  },
+  plugins: [],
+};
